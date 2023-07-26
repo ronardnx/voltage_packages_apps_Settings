fcc2a86e8e80d5489fb6fecd95a03f7d259d0db5
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2023-07-26 09:45:29 +0000

    Disable app copying preference when the user has DISALLOW_INSTALL_APPS restriction
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/users/UserDetailsSettings.java
+++ src/com/android/settings/users/UserDetailsSettings.java
@@ -162,6 +162,7 @@ public class UserDetailsSettings extends SettingsPreferenceFragment
         }
         mAppsInstallsPref.setSummary(UserAppsInstallSettings.getDescription(
                 requireContext(), userRestrictions));
+        mAppCopyingPref.setEnabled(!userRestrictions.isSet(UserManager.DISALLOW_INSTALL_APPS));
     }
 
     @Override
