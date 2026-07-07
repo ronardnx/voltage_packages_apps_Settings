GrapheneOS/platform_packages_apps_Settings

Widevine provisioning setting
Signed-off-by: maade69 <70593890+maade69@users.noreply.github.com>

src/com/android/settings/network/WidevineProvisioningPrefController.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.network;
+
+import android.content.Context;
+import android.ext.settings.ExtSettings;
+import android.ext.settings.WidevineProvisioningSettings;
+
+import com.android.settings.R;
+import androidx.preference.PreferenceScreen;
+import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
+
+public class WidevineProvisioningPrefController extends IntSettingPrefController {
+
+    public WidevineProvisioningPrefController(Context ctx, String key) {
+        super(ctx, key, ExtSettings.WIDEVINE_PROVISIONING_SERVER);
+    }
+
+    @Override
+    public void addPrefsAfterList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.widevine_provisioning_footer);
+    }
+
+    @Override
+    protected void getEntries(Entries entries) {
+        entries.add(R.string.widevine_provisioning_enabled_grapheneos_proxy, WidevineProvisioningSettings.WV_GRAPHENEOS_PROXY);
+        entries.add(R.string.widevine_provisioning_enabled_google_server, WidevineProvisioningSettings.WV_STANDARD_SERVER);
+    }
+}

ronardnx/voltage_packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/settings/network/WidevineProvisioningPrefController.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.network;
+
+import android.content.Context;
+import android.ext.settings.WidevineProvisioningSettings;
+
+import com.android.settings.R;
+import androidx.preference.PreferenceScreen;
+import com.android.settings.ext.IntSettingPrefController;
+import com.android.settings.ext.RadioButtonPickerFragment2;
+
+public class WidevineProvisioningPrefController extends IntSettingPrefController {
+
+    public WidevineProvisioningPrefController(Context ctx, String key) {
+        super(ctx, key, WidevineProvisioningSettings.SERVER_SETTING);
+    }
+
+    @Override
+    public void addPrefsAfterList(RadioButtonPickerFragment2 fragment, PreferenceScreen screen) {
+        addFooterPreference(screen, R.string.widevine_provisioning_footer);
+    }
+
+    @Override
+    protected void getEntries(Entries entries) {
+        entries.add(R.string.widevine_provisioning_enabled_grapheneos_proxy, WidevineProvisioningSettings.WV_GRAPHENEOS_PROXY);
+        entries.add(R.string.widevine_provisioning_enabled_google_server, WidevineProvisioningSettings.WV_STANDARD_SERVER);
+    }
+}