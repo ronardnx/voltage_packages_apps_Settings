DerpFest-LOS/android_packages_apps_Settings

Settings: Implement RefreshRateManager [2/2]
Includes:
- Per-app refresh rate config
- Extreme refresh rate (Force all apps to run in maximum refresh rate)
- Temp refresh rate allowed (Used in MEMC)
- Refresh rate QS tile

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

src/org/nameless/settings/display/refreshrate/ExtremeRefreshRatePreferenceController.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import org.nameless.display.RefreshRateManager;
+
+public class ExtremeRefreshRatePreferenceController extends TogglePreferenceController {
+
+    private final RefreshRateManager mRefreshRateManager;
+
+    public ExtremeRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mRefreshRateManager = context.getSystemService(RefreshRateManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                EXTREME_REFRESH_RATE, 0, UserHandle.USER_CURRENT) == 1;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (!isChecked) {
+            mRefreshRateManager.setExtremeRefreshRateEnabled(false);
+            return true;
+        }
+
+        new AlertDialog.Builder(mContext)
+                .setTitle(mContext.getText(R.string.confirm_before_enable_title))
+                .setMessage(mContext.getText(R.string.extreme_refresh_rate_warning))
+                .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRefreshRateManager.setExtremeRefreshRateEnabled(true);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+        return isChecked();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/org/nameless/settings/display/refreshrate/ScreenRefreshRateFragment.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.display.refreshrate;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+
+public class ScreenRefreshRateFragment extends DashboardFragment {
+
+    private static final String TAG = "ScreenRefreshRateFragment";
+
+    private final SettingsObserver mObserver = new SettingsObserver();
+
+    private final class SettingsObserver extends ContentObserver {
+
+        private final Uri mMinUri = Settings.System.getUriFor(MIN_REFRESH_RATE);
+        private final Uri mPeakUri = Settings.System.getUriFor(PEAK_REFRESH_RATE);
+        private final Uri mExtremeUri = Settings.System.getUriFor(EXTREME_REFRESH_RATE);
+
+        SettingsObserver() {
+            super(new Handler());
+        }
+
+        void register(ContentResolver cr) {
+            cr.registerContentObserver(mMinUri, false, this);
+            cr.registerContentObserver(mPeakUri, false, this);
+            cr.registerContentObserver(mExtremeUri, false, this);
+        }
+
+        void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updatePreferenceStates();
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mObserver.register(getContext().getContentResolver());
+    }
+
+    @Override
+    public void onDestroy() {
+        mObserver.unregister(getContext().getContentResolver());
+        super.onDestroy();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.screen_refresh_rate;
+    }
+}

src/org/nameless/settings/fragment/PerAppConfigFragment.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.fragment;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Pair;
+import android.view.View;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.widget.EmptyTextSettings;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.nameless.settings.widget.AppListPreference;
+
+public abstract class PerAppConfigFragment extends EmptyTextSettings {
+
+    private Context mContext;
+    private PackageManager mPackageManager;
+
+    /**
+     * Comparator by label, if null or empty then packageName.
+     */
+    static class AppComparator implements Comparator<Pair<String, String>> {
+
+        private final Collator mCollator = Collator.getInstance();
+
+        public final int compare(Pair<String, String> a,
+                Pair<String, String> b) {
+            String sa = a.first;
+            if (TextUtils.isEmpty(sa)) sa = a.second;
+            String sb = b.first;
+            if (TextUtils.isEmpty(sb)) sb = b.second;
+            return mCollator.compare(sa, sb);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mContext = getActivity();
+        mPackageManager = mContext.getPackageManager();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Clear the prefs
+        final PreferenceScreen screen = getPreferenceScreen();
+        screen.removeAll();
+
+        final ArrayList<Pair<String, String>> apps = collectApps();
+
+        // Rebuild the list of prefs
+        final Context prefContext = getPrefContext();
+        for (final Pair<String, String> appData : apps) {
+            final ListPreference pref = new AppListPreference(prefContext);
+            pref.setIcon(getIcon(appData.second));
+            pref.setTitle(appData.first);
+            pref.setDialogTitle(appData.first);
+
+            int size = getEntries().size();
+
+            CharSequence[] entries = new CharSequence[size];
+            CharSequence[] values = new CharSequence[size];
+            for (int i = 0; i < size; ++i) {
+                entries[i] = getEntries().get(i);
+                values[i] = String.valueOf(getValues().get(i));
+            }
+            pref.setEntries(entries);
+            pref.setEntryValues(values);
+
+            final String value = String.valueOf(getCurrentValue(appData.second));
+            CharSequence summary = "-1";
+            for (int i = 0; i < size; ++i) {
+                if (values[i].equals(value)) {
+                    summary = entries[i];
+                    break;
+                }
+            }
+            pref.setValue(value);
+            pref.setSummary(summary);
+
+            pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    final String newValueStr = String.valueOf(newValue);
+                    onValueChanged(appData.second, Integer.parseInt(newValueStr));
+                    CharSequence[] entries = pref.getEntries();
+                    CharSequence[] values = pref.getEntryValues();
+                    CharSequence summary = entries[0];
+                    for (int i = 0; i < entries.length; ++i) {
+                        if (values[i].equals(newValueStr)) {
+                            summary = entries[i];
+                            break;
+                        }
+                    }
+                    pref.setSummary(summary);
+                    return true;
+                }
+            });
+            screen.addPreference(pref);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setEmptyText(R.string.per_app_config_empty_text);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    /**
+     * @return the sorted list of pair<label, packageName> of all applications for current user,
+     * with extra system applications defined in R.array.config_perAppConfAllowedSystemApps.
+     */
+    private ArrayList<Pair<String, String>> collectApps() {
+        final ArrayList<Pair<String, String>> apps = new ArrayList<>();
+        final List<PackageInfo> installedPackages =
+                mPackageManager.getInstalledPackages(0);
+        for (PackageInfo pi : installedPackages) {
+            if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                continue;
+            }
+            apps.add(new Pair<>(
+                    pi.applicationInfo.loadLabel(mPackageManager).toString(),
+                    pi.packageName));
+        }
+        final String[] systemApps = mContext.getResources().getStringArray(
+                R.array.config_perAppConfAllowedSystemApps);
+        for (String app : systemApps) {
+            try {
+                final PackageInfo pi = mPackageManager.getPackageInfo(app, 0);
+                pi.applicationInfo.loadLabel(mPackageManager).toString();
+                apps.add(new Pair<>(
+                        pi.applicationInfo.loadLabel(mPackageManager).toString(), app));
+            } catch (NameNotFoundException e) {
+            }
+        }
+        Collections.sort(apps, new AppComparator());
+        return apps;
+    }
+
+    private Drawable getIcon(String packageName) {
+        Drawable loadIcon = null;
+        if (packageName != null) {
+            try {
+                loadIcon = mPackageManager.getApplicationIcon(packageName);
+            } catch (NameNotFoundException e) {
+            }
+        }
+        return loadIcon != null ? loadIcon : mPackageManager.getDefaultActivityIcon();
+    }
+
+    protected abstract List<String> getEntries();
+
+    protected abstract List<Integer> getValues();
+
+    protected abstract int getCurrentValue(String packageName);
+
+    protected abstract void onValueChanged(String packageName, int value);
+}

src/org/nameless/settings/widget/AppListPreference.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.ListPreference;
+
+import com.android.settings.R;
+
+/**
+ * The ListPreference for the pages need to show apps icon.
+*/
+public class AppListPreference extends ListPreference {
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_app);
+    }
+}

ronardnx/voltage_packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/voltage/settings/display/refreshrate/ExtremeRefreshRatePreferenceController.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.display.refreshrate;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.appcompat.app.AlertDialog;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import com.voltage.display.RefreshRateManager;
+
+public class ExtremeRefreshRatePreferenceController extends TogglePreferenceController {
+
+    private final RefreshRateManager mRefreshRateManager;
+
+    public ExtremeRefreshRatePreferenceController(Context context, String key) {
+        super(context, key);
+        mRefreshRateManager = context.getSystemService(RefreshRateManager.class);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                EXTREME_REFRESH_RATE, 0, UserHandle.USER_CURRENT) == 1;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        if (!isChecked) {
+            mRefreshRateManager.setExtremeRefreshRateEnabled(false);
+            return true;
+        }
+
+        new AlertDialog.Builder(mContext)
+                .setTitle(mContext.getText(R.string.confirm_before_enable_title))
+                .setMessage(mContext.getText(R.string.extreme_refresh_rate_warning))
+                .setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mRefreshRateManager.setExtremeRefreshRateEnabled(true);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+        return isChecked();
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return 0;
+    }
+}

src/com/voltage/settings/display/refreshrate/ScreenRefreshRateFragment.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.display.refreshrate;
+
+import static android.provider.Settings.System.MIN_REFRESH_RATE;
+import static android.provider.Settings.System.PEAK_REFRESH_RATE;
+
+import static android.provider.Settings.System.EXTREME_REFRESH_RATE;
+
+import android.app.settings.SettingsEnums;
+import android.content.ContentResolver;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+
+public class ScreenRefreshRateFragment extends DashboardFragment {
+
+    private static final String TAG = "ScreenRefreshRateFragment";
+
+    private final SettingsObserver mObserver = new SettingsObserver();
+
+    private final class SettingsObserver extends ContentObserver {
+
+        private final Uri mMinUri = Settings.System.getUriFor(MIN_REFRESH_RATE);
+        private final Uri mPeakUri = Settings.System.getUriFor(PEAK_REFRESH_RATE);
+        private final Uri mExtremeUri = Settings.System.getUriFor(EXTREME_REFRESH_RATE);
+
+        SettingsObserver() {
+            super(new Handler());
+        }
+
+        void register(ContentResolver cr) {
+            cr.registerContentObserver(mMinUri, false, this);
+            cr.registerContentObserver(mPeakUri, false, this);
+            cr.registerContentObserver(mExtremeUri, false, this);
+        }
+
+        void unregister(ContentResolver cr) {
+            cr.unregisterContentObserver(this);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            updatePreferenceStates();
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        mObserver.register(getContext().getContentResolver());
+    }
+
+    @Override
+    public void onDestroy() {
+        mObserver.unregister(getContext().getContentResolver());
+        super.onDestroy();
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.screen_refresh_rate;
+    }
+}

src/com/voltage/settings/fragment/PerAppConfigFragment.java
@@ -1,0 +1,188 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.fragment;
+
+import android.app.settings.SettingsEnums;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Pair;
+import android.view.View;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.widget.EmptyTextSettings;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.voltage.settings.widget.AppListPreference;
+
+public abstract class PerAppConfigFragment extends EmptyTextSettings {
+
+    private Context mContext;
+    private PackageManager mPackageManager;
+
+    /**
+     * Comparator by label, if null or empty then packageName.
+     */
+    static class AppComparator implements Comparator<Pair<String, String>> {
+
+        private final Collator mCollator = Collator.getInstance();
+
+        public final int compare(Pair<String, String> a,
+                Pair<String, String> b) {
+            String sa = a.first;
+            if (TextUtils.isEmpty(sa)) sa = a.second;
+            String sb = b.first;
+            if (TextUtils.isEmpty(sb)) sb = b.second;
+            return mCollator.compare(sa, sb);
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        mContext = getActivity();
+        mPackageManager = mContext.getPackageManager();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Clear the prefs
+        final PreferenceScreen screen = getPreferenceScreen();
+        screen.removeAll();
+
+        final ArrayList<Pair<String, String>> apps = collectApps();
+
+        // Rebuild the list of prefs
+        final Context prefContext = getPrefContext();
+        for (final Pair<String, String> appData : apps) {
+            final ListPreference pref = new AppListPreference(prefContext);
+            pref.setIcon(getIcon(appData.second));
+            pref.setTitle(appData.first);
+            pref.setDialogTitle(appData.first);
+
+            int size = getEntries().size();
+
+            CharSequence[] entries = new CharSequence[size];
+            CharSequence[] values = new CharSequence[size];
+            for (int i = 0; i < size; ++i) {
+                entries[i] = getEntries().get(i);
+                values[i] = String.valueOf(getValues().get(i));
+            }
+            pref.setEntries(entries);
+            pref.setEntryValues(values);
+
+            final String value = String.valueOf(getCurrentValue(appData.second));
+            CharSequence summary = "-1";
+            for (int i = 0; i < size; ++i) {
+                if (values[i].equals(value)) {
+                    summary = entries[i];
+                    break;
+                }
+            }
+            pref.setValue(value);
+            pref.setSummary(summary);
+
+            pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    final String newValueStr = String.valueOf(newValue);
+                    onValueChanged(appData.second, Integer.parseInt(newValueStr));
+                    CharSequence[] entries = pref.getEntries();
+                    CharSequence[] values = pref.getEntryValues();
+                    CharSequence summary = entries[0];
+                    for (int i = 0; i < entries.length; ++i) {
+                        if (values[i].equals(newValueStr)) {
+                            summary = entries[i];
+                            break;
+                        }
+                    }
+                    pref.setSummary(summary);
+                    return true;
+                }
+            });
+            screen.addPreference(pref);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setEmptyText(R.string.per_app_config_empty_text);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return SettingsEnums.PAGE_UNKNOWN;
+    }
+
+    /**
+     * @return the sorted list of pair<label, packageName> of all applications for current user,
+     * with extra system applications defined in R.array.config_perAppConfAllowedSystemApps.
+     */
+    private ArrayList<Pair<String, String>> collectApps() {
+        final ArrayList<Pair<String, String>> apps = new ArrayList<>();
+        final List<PackageInfo> installedPackages =
+                mPackageManager.getInstalledPackages(0);
+        for (PackageInfo pi : installedPackages) {
+            if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0) {
+                continue;
+            }
+            apps.add(new Pair<>(
+                    pi.applicationInfo.loadLabel(mPackageManager).toString(),
+                    pi.packageName));
+        }
+        final String[] systemApps = mContext.getResources().getStringArray(
+                R.array.config_perAppConfAllowedSystemApps);
+        for (String app : systemApps) {
+            try {
+                final PackageInfo pi = mPackageManager.getPackageInfo(app, 0);
+                pi.applicationInfo.loadLabel(mPackageManager).toString();
+                apps.add(new Pair<>(
+                        pi.applicationInfo.loadLabel(mPackageManager).toString(), app));
+            } catch (NameNotFoundException e) {
+            }
+        }
+        Collections.sort(apps, new AppComparator());
+        return apps;
+    }
+
+    private Drawable getIcon(String packageName) {
+        Drawable loadIcon = null;
+        if (packageName != null) {
+            try {
+                loadIcon = mPackageManager.getApplicationIcon(packageName);
+            } catch (NameNotFoundException e) {
+            }
+        }
+        return loadIcon != null ? loadIcon : mPackageManager.getDefaultActivityIcon();
+    }
+
+    protected abstract List<String> getEntries();
+
+    protected abstract List<Integer> getValues();
+
+    protected abstract int getCurrentValue(String packageName);
+
+    protected abstract void onValueChanged(String packageName, int value);
+}

src/com/voltage/settings/widget/AppListPreference.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.ListPreference;
+
+import com.android.settings.R;
+
+/**
+ * The ListPreference for the pages need to show apps icon.
+*/
+public class AppListPreference extends ListPreference {
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_app);
+    }
+}