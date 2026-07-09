SunOS-Project/android_sun-sdk

Settings: Create PerAppSwitchConfigFragment
Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

Settings/src/org/nameless/settings/fragment/PerAppSwitchConfigFragment.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.fragment;
+
+import android.content.Context;
+import android.util.Pair;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashMap;
+
+import org.nameless.custom.preference.SwitchPreference;
+import org.nameless.settings.widget.AppSwitchPreference;
+
+public abstract class PerAppSwitchConfigFragment extends BasePerAppConfigFragment {
+
+    private final LinkedHashMap<String, Boolean> mPkgCheckState = new LinkedHashMap<>();
+
+    @Override
+    protected Preference createAppPreference(Context prefContext, Pair<String, String> appData) {
+        final SwitchPreference pref = new AppSwitchPreference(prefContext);
+        pref.setIcon(getIcon(appData.second));
+        pref.setTitle(appData.first);
+        pref.setSummary(appData.second);
+
+        final boolean checked = isChecked(appData.second);
+        pref.setChecked(checked);
+        mPkgCheckState.put(appData.second, checked);
+
+        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final boolean ret = onSetChecked(appData.second, (Boolean) newValue);
+                if (ret) {
+                    mPkgCheckState.put(appData.second, (Boolean) newValue);
+                    onCheckedListUpdated(generateCheckedPkgList());
+                }
+                return ret;
+            }
+        });
+
+        return pref;
+    }
+
+    private List<String> generateCheckedPkgList() {
+        final List<String> ret = new ArrayList<>();
+        for (String pkg : mPkgCheckState.keySet()) {
+            if (mPkgCheckState.get(pkg)) {
+                ret.add(pkg);
+            }
+        }
+        return ret;
+    }
+
+    protected abstract boolean isChecked(String packageName);
+
+    protected abstract boolean onSetChecked(String packageName, boolean checked);
+
+    protected abstract void onCheckedListUpdated(List<String> pkgList);
+}

Settings/src/org/nameless/settings/widget/AppSwitchPreference.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.nameless.settings.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.android.settings.R;
+
+import org.nameless.custom.preference.SwitchPreference;
+
+/**
+ * The SwitchPreference for the pages need to show apps icon.
+*/
+public class AppSwitchPreference extends SwitchPreference {
+
+    public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_app);
+    }
+}

ronardnx/voltage_packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/voltage/settings/fragment/PerAppSwitchConfigFragment.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.fragment;
+
+import android.content.Context;
+import android.util.Pair;
+
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedHashMap;
+
+import com.voltage.settings.widget.AppSwitchPreference;
+
+public abstract class PerAppSwitchConfigFragment extends BasePerAppConfigFragment {
+
+    private final LinkedHashMap<String, Boolean> mPkgCheckState = new LinkedHashMap<>();
+
+    @Override
+    protected Preference createAppPreference(Context prefContext, Pair<String, String> appData) {
+        final SwitchPreference pref = new AppSwitchPreference(prefContext);
+        pref.setIcon(getIcon(appData.second));
+        pref.setTitle(appData.first);
+        pref.setSummary(appData.second);
+
+        final boolean checked = isChecked(appData.second);
+        pref.setChecked(checked);
+        mPkgCheckState.put(appData.second, checked);
+
+        pref.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                final boolean ret = onSetChecked(appData.second, (Boolean) newValue);
+                if (ret) {
+                    mPkgCheckState.put(appData.second, (Boolean) newValue);
+                    onCheckedListUpdated(generateCheckedPkgList());
+                }
+                return ret;
+            }
+        });
+
+        return pref;
+    }
+
+    private List<String> generateCheckedPkgList() {
+        final List<String> ret = new ArrayList<>();
+        for (String pkg : mPkgCheckState.keySet()) {
+            if (mPkgCheckState.get(pkg)) {
+                ret.add(pkg);
+            }
+        }
+        return ret;
+    }
+
+    protected abstract boolean isChecked(String packageName);
+
+    protected abstract boolean onSetChecked(String packageName, boolean checked);
+
+    protected abstract void onCheckedListUpdated(List<String> pkgList);
+}

src/com/voltage/settings/widget/AppSwitchPreference.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2023 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.voltage.settings.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+
+/**
+ * The SwitchPreference for the pages need to show apps icon.
+*/
+public class AppSwitchPreference extends SwitchPreference {
+
+    public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context) {
+        super(context);
+        setLayoutResource(R.layout.preference_app);
+    }
+
+    public AppSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.preference_app);
+    }
+}