DerpFest-LOS/android_packages_apps_Settings

Settings: Implement DisplayResolutionManager [2/2]
Ref: https://github.com/SunOS-Project/android_packages_apps_Settings/commit/f121d870b0c9bfb0fa6d26f03c5d7ad4e6ae6098

Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

src/com/android/settings/display/ScreenResolutionController.java
@@ -16,6 +16,8 @@
 
 package com.android.settings.display;
 
+import static org.derpfest.view.DisplayResolutionManager.TYPE_DISABLED;
+
 import android.content.Context;
 import android.graphics.Point;
 import android.hardware.display.DisplayManager;
@@ -33,11 +35,18 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.derpfest.view.DisplayResolutionManager;
+
 /** Controller that switch the screen resolution. */
 public class ScreenResolutionController extends BasePreferenceController {
     private static final String TAG = "ScreenResolutionController";
     static final int HIGHRESOLUTION_IDX = 0;
     static final int FULLRESOLUTION_IDX = 1;
+
+    static final boolean CUSTOM_RESOLUTION_SWITCHER =
+            DisplayResolutionManager.getDeviceType() != TYPE_DISABLED;
+
+    private final DisplayResolutionManager mDisplayResolutionManager;
 
     private Display mDisplay;
     private Set<Point> mSupportedResolutions = null;
@@ -48,6 +57,8 @@ public class ScreenResolutionController extends BasePreferenceController {
 
     public ScreenResolutionController(Context context, String key) {
         super(context, key);
+
+        mDisplayResolutionManager = mContext.getSystemService(DisplayResolutionManager.class);
 
         mDisplay =
                 mContext.getSystemService(DisplayManager.class).getDisplay(Display.DEFAULT_DISPLAY);
@@ -88,7 +99,7 @@ public class ScreenResolutionController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return (checkSupportedResolutions()) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return (CUSTOM_RESOLUTION_SWITCHER || checkSupportedResolutions()) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -131,8 +142,18 @@ public class ScreenResolutionController extends BasePreferenceController {
         return this.mFullHeight;
     }
 
+    public DisplayResolutionManager getDisplayResolutionManager() {
+        return mDisplayResolutionManager;
+    }
+
     @VisibleForTesting
     public int getDisplayWidth() {
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            final int width = mDisplayResolutionManager.getDisplayResolution().x;
+            if (width > 0) {
+                return width;
+            }
+        }
         return mDisplay.getMode().getPhysicalWidth();
     }
 

src/com/android/settings/display/ScreenResolutionFragment.java
@@ -16,6 +16,10 @@
 
 package com.android.settings.display;
 
+import static com.android.settings.display.ScreenResolutionController.CUSTOM_RESOLUTION_SWITCHER;
+
+import static org.derpfest.view.DisplayResolutionManager.RESTART_SYSTEMUI_ON_SWITCH;
+
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.res.Resources;
@@ -49,6 +53,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.derpfest.view.DisplayResolutionManager;
+
 /** Preference fragment used for switch screen resolution */
 @SearchIndexable
 public class ScreenResolutionFragment extends RadioButtonPickerFragment {
@@ -61,6 +67,8 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
     private String[] mScreenResolutionOptions;
     private Set<Point> mResolutions;
     private String[] mScreenResolutionSummaries;
+
+    private DisplayResolutionManager mDisplayResolutionManager;
 
     private IllustrationPreference mImagePreference;
     private DisplayObserver mDisplayObserver;
@@ -86,6 +94,7 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
         mResolutions = controller.getAllSupportedResolutions();
         mHighWidth = controller.getHighWidth();
         mFullWidth = controller.getFullWidth();
+        mDisplayResolutionManager = controller.getDisplayResolutionManager();
         Log.i(TAG, "mHighWidth:" + mHighWidth + "mFullWidth:" + mFullWidth);
         mScreenResolutionSummaries =
                 new String[] {
@@ -105,7 +114,8 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
         screen.addPreference(mImagePreference);
 
         final FooterPreference footerPreference = new FooterPreference(screen.getContext());
-        footerPreference.setTitle(R.string.screen_resolution_footer);
+        footerPreference.setTitle(RESTART_SYSTEMUI_ON_SWITCH ?
+                R.string.screen_resolution_footer_custom : R.string.screen_resolution_footer);
         footerPreference.setSelectable(false);
         footerPreference.setLayoutResource(
                 com.android.settingslib.widget.preference.footer.R.layout.preference_footer);
@@ -210,6 +220,13 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
 
     @Override
     protected String getDefaultKey() {
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            final int width = mDisplayResolutionManager.getDisplayResolution().x;
+            if (width > 0) {
+                return getKeyForResolution(width);
+            }
+        }
+
         int physicalWidth = getDisplayMode().getPhysicalWidth();
 
         return getKeyForResolution(physicalWidth);
@@ -222,7 +239,11 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
             return false;
         }
 
-        setDisplayMode(width);
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            mDisplayResolutionManager.setDisplayResolution(width);
+        } else {
+            setDisplayMode(width);
+        }
         updateIllustrationImage(mImagePreference);
 
         return true;

ronardnx/voltage_packages_apps_Settings

Signed-off-by: Dmitrii <bankersenator@gmail.com>

src/com/android/settings/display/ScreenResolutionController.java
@@ -16,6 +16,8 @@
 
 package com.android.settings.display;
 
+import static com.voltage.view.DisplayResolutionManager.TYPE_DISABLED;
+
 import android.content.Context;
 import android.graphics.Point;
 import android.hardware.display.DisplayManager;
@@ -33,12 +35,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.voltage.view.DisplayResolutionManager;
+
 // LINT.IfChange
 /** Controller that switch the screen resolution. */
 public class ScreenResolutionController extends BasePreferenceController {
     private static final String TAG = "ScreenResolutionController";
     static final int HIGHRESOLUTION_IDX = 0;
     static final int FULLRESOLUTION_IDX = 1;
+
+    static final boolean CUSTOM_RESOLUTION_SWITCHER =
+            DisplayResolutionManager.getDeviceType() != TYPE_DISABLED;
+
+    private final DisplayResolutionManager mDisplayResolutionManager;
 
     private Display mDisplay;
     private Set<Point> mSupportedResolutions = null;
@@ -49,6 +58,8 @@ public class ScreenResolutionController extends BasePreferenceController {
 
     public ScreenResolutionController(Context context, String key) {
         super(context, key);
+
+        mDisplayResolutionManager = mContext.getSystemService(DisplayResolutionManager.class);
 
         mDisplay =
                 mContext.getSystemService(DisplayManager.class).getDisplay(Display.DEFAULT_DISPLAY);
@@ -84,9 +95,7 @@ public class ScreenResolutionController extends BasePreferenceController {
 
     @Override
     public int getAvailabilityStatus() {
-        return ScreenResolutionExtensionsKt.checkSupportedResolutions(mContext)
-                ? AVAILABLE
-                : UNSUPPORTED_ON_DEVICE;
+        return (CUSTOM_RESOLUTION_SWITCHER || ScreenResolutionExtensionsKt.checkSupportedResolutions(mContext)) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
@@ -129,8 +138,18 @@ public class ScreenResolutionController extends BasePreferenceController {
         return this.mFullHeight;
     }
 
+    public DisplayResolutionManager getDisplayResolutionManager() {
+        return mDisplayResolutionManager;
+    }
+
     @VisibleForTesting
     public int getDisplayWidth() {
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            final int width = mDisplayResolutionManager.getDisplayResolution().x;
+            if (width > 0) {
+                return width;
+            }
+        }
         return mDisplay.getMode().getPhysicalWidth();
     }
 

src/com/android/settings/display/ScreenResolutionFragment.java
@@ -16,6 +16,10 @@
 
 package com.android.settings.display;
 
+import static com.android.settings.display.ScreenResolutionController.CUSTOM_RESOLUTION_SWITCHER;
+
+import static com.voltage.view.DisplayResolutionManager.RESTART_SYSTEMUI_ON_SWITCH;
+
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.res.Resources;
@@ -47,6 +51,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.voltage.view.DisplayResolutionManager;
+
 /** Preference fragment used for switch screen resolution */
 // LINT.IfChange
 @SearchIndexable
@@ -60,6 +66,8 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
     private String[] mScreenResolutionOptions;
     private Set<Point> mResolutions;
     private SpannableString[] mScreenResolutionSummaries;
+
+    private DisplayResolutionManager mDisplayResolutionManager;
 
     private IllustrationPreference mImagePreference;
     private DisplayObserver mDisplayObserver;
@@ -83,6 +91,7 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
         mResolutions = controller.getAllSupportedResolutions();
         mHighWidth = ScreenResolutionExtensionsKt.getHighResolutionDisplayWidth(context);
         mFullWidth = ScreenResolutionExtensionsKt.getFullResolutionDisplayWidth(context);
+        mDisplayResolutionManager = controller.getDisplayResolutionManager();
         Log.i(TAG, "mHighWidth:" + mHighWidth + "mFullWidth:" + mFullWidth);
         mScreenResolutionSummaries =
                 new SpannableString[] {
@@ -109,7 +118,8 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
     @Override
     protected void addStaticPreferences(PreferenceScreen screen) {
         final FooterPreference footerPreference = new FooterPreference(screen.getContext());
-        footerPreference.setTitle(R.string.screen_resolution_footer);
+        footerPreference.setTitle(RESTART_SYSTEMUI_ON_SWITCH ?
+                R.string.screen_resolution_footer_custom : R.string.screen_resolution_footer);
         footerPreference.setSelectable(false);
         footerPreference.setLayoutResource(
                 com.android.settingslib.widget.preference.footer.R.layout.preference_footer);
@@ -178,6 +188,13 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
 
     @Override
     protected String getDefaultKey() {
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            final int width = mDisplayResolutionManager.getDisplayResolution().x;
+            if (width > 0) {
+                return getKeyForResolution(width);
+            }
+        }
+
         int physicalWidth = getDisplayMode().getPhysicalWidth();
 
         return getKeyForResolution(physicalWidth);
@@ -190,7 +207,11 @@ public class ScreenResolutionFragment extends RadioButtonPickerFragment {
             return false;
         }
 
-        setDisplayMode(width);
+        if (CUSTOM_RESOLUTION_SWITCHER) {
+            mDisplayResolutionManager.setDisplayResolution(width);
+        } else {
+            setDisplayMode(width);
+        }
 
         return true;
     }